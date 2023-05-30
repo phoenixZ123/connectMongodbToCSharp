138c175b0bbdfb8f020da6b675b6b1a5901c08bf
phu nov <phuephue1125@gmail.com>
2023-05-30 13:25:42 +0630

    first commit

---
+++ MongodbMyself/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Configuration;
+using MongodbMyself;
+using System;
+using System.Data.Common;
+
+string ConnectionString = "mongodb+srv://pwintphoowai:mongoose@phoo.glozq0x.mongodb.net/user?retryWrites=true&w=majority";
+string databaseName = "simple_db";
+string collectionName = "user";
+
+var client =new MongoClient(ConnectionString);
+var database = client.GetDatabase(databaseName);
+//get table
+var collection = database.GetCollection<UserModel>(collectionName);
+
+//store data to mongodb
+var insert = new UserModel { Name = "PwintPhooWai", Type = "Reader" };
+
+await collection.InsertOneAsync(insert);
+
+//await is scceptable code impt
+var results =await collection.FindAsync(_ => true);
+
+foreach (var data in results.ToList())
+{
+    Console.WriteLine($"{data.Id}: {data.Name} {data.Type}");
+}
+
---
+++ MongodbMyself/UserModel.cs
@@ -0,0 +1,16 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System.ComponentModel.DataAnnotations;
+
+namespace MongodbMyself;
+
+public class UserModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+}
